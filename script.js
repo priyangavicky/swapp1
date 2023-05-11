6cc41156195530035c90fd8f01a8c75e9f5ea9aa
priyangavicky <110669229+priyangavicky@users.noreply.github.com>
2023-05-11 15:40:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function swap1(){
+    var displayImage=document.getElementById('screen').src;
+    var img1=document.getElementById('image1').src;
+    document.getElementById('image1').src=displayImage;
+    document.getElementById('screen').src=img1;
+}
+function swap2(){
+    var displayImage=document.getElementById('screen').src;
+    var img2=document.getElementById('image2').src;
+    document.getElementById('image2').src=displayImage;
+    document.getElementById('screen').src=img2;
+}
+function swap3(){
+    var displayImage=document.getElementById('screen').src;
+    var img3=document.getElementById('image3').src;
+    document.getElementById('image3').src=displayImage;
+    document.getElementById('screen').src=img3;
+}
+function swap4(){
+    var displayImage=document.getElementById('screen').src;
+    var img4=document.getElementById('image4').src;
+    document.getElementById('image4').src=displayImage;
+    document.getElementById('screen').src=img4;
+}
